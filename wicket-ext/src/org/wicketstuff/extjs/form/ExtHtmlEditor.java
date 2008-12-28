558b03a86c8e1f6954c1c2e0a5427e830251fa91
paolo.ditommaso <paolo.ditommaso@fab100c0-a45f-11dd-a9c7-49dec45ca499>
2008-12-28 16:01:32 +0000

    improvements & licence headers - ver 0.6

--- wicket-ext/src/org/wicketstuff/extjs/form/ExtHtmlEditor.java
+++ wicket-ext/src/org/wicketstuff/extjs/form/ExtHtmlEditor.java
@@ -1,3 +1,19 @@
+/*
+ *  Copyright 2008 Wicket-Ext
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.wicketstuff.extjs.form;
 
 import org.apache.wicket.markup.ComponentTag;
@@ -10,12 +26,12 @@ import org.wicketstuff.extjs.behavior.ExtComponentBehavior;
 public class ExtHtmlEditor extends AbstractTextComponent {
 
 	private Config extOptions = new Config();
-	
+
 	public ExtHtmlEditor(String id) {
 		super(id);
 		init();
 	}
-	
+
 	public ExtHtmlEditor(String id,Config options) {
 		super(id);
 		this.extOptions = options;
@@ -36,7 +52,7 @@ public class ExtHtmlEditor extends AbstractTextComponent {
 	{
 		replaceComponentTagBody(markupStream, openTag, "");
 	}
-	
+
 }
 
 
@@ -44,21 +60,21 @@ class ExtHtmlEditorBehavior extends ExtComponentBehavior {
 
 
 	@Override
-	public void onBind() { 
+	public void onBind() {
 		super.onBind();
-		
-		if( !(getComponent() instanceof AbstractTextComponent) ) { 
+
+		if( !(getComponent() instanceof AbstractTextComponent) ) {
 			throw new RuntimeException("ExtHtmlEditorBehavior can be applyed only to AbstractTextComponent or subclass");
 		}
 	}
-	
+
 	public ExtHtmlEditorBehavior() {
 		super("Ext.form.HtmlEditor");
 	}
-	
+
 	public ExtHtmlEditorBehavior(Config options) {
 		super("Ext.form.HtmlEditor");
-		super.options().putAll(options);
+		super.config().putAll(options);
 	}
 
 	@Override
@@ -66,21 +82,21 @@ class ExtHtmlEditorBehavior extends ExtComponentBehavior {
 	{
 		super.onComponentTag(tag);
 		tag.setName("div");
-		tag.getAttributes().remove("name");		
+		tag.getAttributes().remove("name");
 	}
-	
+
 	@Override
-	protected String getApplyMethod() { 
+	protected String getApplyMethod() {
 		return "renderTo";
 	}
-		
-	
+
+
 	@Override
-	protected void onExtConfig( Config config ) { 
+	protected void onExtConfig( Config config ) {
 		AbstractTextComponent comp = (AbstractTextComponent) getComponent();
 		config.set("name", comp.getId() );
 		config.set("value", comp.getModelObject());
 	}
-	
-	
+
+
 }
--- wicket-ext/src/org/wicketstuff/extjs/form/ExtTextField.java
+++ wicket-ext/src/org/wicketstuff/extjs/form/ExtTextField.java
@@ -1,3 +1,19 @@
+/*
+ *  Copyright 2008 Wicket-Ext
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.wicketstuff.extjs.form;
 
 import org.apache.wicket.markup.html.form.TextField;
@@ -6,7 +22,7 @@ import org.wicketstuff.extjs.behavior.ExtTextFieldBehavior;
 
 /**
  * Generic Text field integrating the Ext <code>Ext.form.TextField</code>
- * 
+ *
  * @author Paolo Di Tommaso
  *
  */
@@ -16,13 +32,13 @@ public class ExtTextField extends TextField {
 		super(id);
 		init();
 	}
-	
-	public ExtTextField( String id, IModel model ) { 
+
+	public ExtTextField( String id, IModel model ) {
 		super(id,model);
 		init();
 	}
-	
-	private void init() { 
+
+	private void init() {
 		add( new ExtTextFieldBehavior() );
 	}
 
