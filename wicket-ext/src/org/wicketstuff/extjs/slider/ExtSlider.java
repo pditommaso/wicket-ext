8a0e8c7bba624be503012b4128ba4f2f39bd9cc9
paolo.ditommaso <paolo.ditommaso@fab100c0-a45f-11dd-a9c7-49dec45ca499>
2008-12-28 16:02:41 +0000

    improvements & licence headers - ver 0.6

--- wicket-ext/src/org/wicketstuff/extjs/slider/ExtSlider.java
+++ wicket-ext/src/org/wicketstuff/extjs/slider/ExtSlider.java
@@ -1,3 +1,19 @@
+/*
+ *  Copyright 2008 Wicket-Ext
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.wicketstuff.extjs.slider;
 
 import java.util.HashMap;
@@ -21,9 +37,9 @@ public class ExtSlider extends WebMarkupContainer {
 
 	private static final long serialVersionUID = 1L;
 	private static final String VALUE_PARAM = "newValue";
-	
+
 	private Config config ;
-	
+
 	{
 		config = new Config();
 	}
@@ -37,79 +53,79 @@ public class ExtSlider extends WebMarkupContainer {
 		super(id, model);
 		init();
 	}
-	
+
 	private void init() {
-		add( new ExtSliderBehavior(config) );		
+		add( new ExtSliderBehavior(config) );
 	}
-	
-	public ExtSlider setWidth( int width ) { 
+
+	public ExtSlider setWidth( int width ) {
 		config.set("width", width );
 		return this;
 	}
-	
-	public ExtSlider setHeight( int height ) { 
+
+	public ExtSlider setHeight( int height ) {
 		config.set("height", height );
 		return this;
 	}
-	
-	public ExtSlider setMinValue( int minValue ) { 
+
+	public ExtSlider setMinValue( int minValue ) {
 		config.set("minValue", minValue );
 		return this;
 	}
-	
-	public ExtSlider setMaxValue( int maxValue ) { 
+
+	public ExtSlider setMaxValue( int maxValue ) {
 		config.set("maxValue", maxValue );
 		return this;
 	}
-	
-	public ExtSlider setIncrement( int increment ) { 
+
+	public ExtSlider setIncrement( int increment ) {
 		config.set("increment", increment );
 		return this;
 	}
-	
-	public ExtSlider setVertical( boolean vertical ) { 
+
+	public ExtSlider setVertical( boolean vertical ) {
 		config.set("vertical", vertical );
 		return this;
 	}
-	
-	public ExtSlider setValue( int value ) { 
+
+	public ExtSlider setValue( int value ) {
 		setModelObject(value);
 		return this;
 	}
-	
+
 	protected void onChange(int oldValue,int newValue, AjaxRequestTarget target) {
-        
+
     }
-	
-	class ExtSliderBehavior extends ExtComponentBehavior { 
-		
+
+	class ExtSliderBehavior extends ExtComponentBehavior {
+
 		private static final long serialVersionUID = 1L;
-		
+
 		public ExtSliderBehavior(Config config) {
 			super("Ext.Slider",config);
 		}
 
 		@Override
-		protected String getApplyMethod() { 
+		protected String getApplyMethod() {
 			return "renderTo";
 		}
-		
+
 		@Override
 		public void renderHead(IHeaderResponse response) {
 	        super.renderHead(response);
 	        response.renderJavascriptReference(WicketAjaxReference.INSTANCE);
 		}
-		
+
 		@Override
-		protected CharSequence onExtScript(Config config) { 
+		protected CharSequence onExtScript(Config config) {
 			config.set("value", new ExtLiteral(getModelObjectAsString()));
 			return super.onExtScript(config);
 		}
-		
+
 		@Override
 	    protected void onExtConfig( Config config ) {
 	        String url = getCallbackUrl().toString();
-	        
+
 	        Map<String,Object> params = new HashMap<String, Object>();
 	        params.put(VALUE_PARAM, Ext.literal("newValue"));
 	        WicketCallBuilder ajax = new WicketCallBuilder(url);
