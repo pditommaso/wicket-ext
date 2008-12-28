84bfac0362de687e0596aecc95121f729f72b9d4
paolo.ditommaso <paolo.ditommaso@fab100c0-a45f-11dd-a9c7-49dec45ca499>
2008-12-28 16:00:12 +0000

    improvements & licence headers - ver 0.6

--- wicket-ext/src/org/wicketstuff/extjs/dialog/ExtMessageDialog.java
+++ wicket-ext/src/org/wicketstuff/extjs/dialog/ExtMessageDialog.java
@@ -1,3 +1,19 @@
+/*
+ *  Copyright 2008 Wicket-Ext
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.wicketstuff.extjs.dialog;
 
 import org.apache.wicket.ajax.AjaxRequestTarget;
@@ -8,7 +24,7 @@ import org.wicketstuff.extjs.Ext;
 
 /**
  * Integrate the <code>Ext.MessageBox</code> dialogs with Wicket
- * 
+ *
  * @author Paolo Di Tommaso
  *
  */
@@ -18,72 +34,72 @@ public class ExtMessageDialog extends WebMarkupContainer {
 	public static final String BTN_NO = "no";
 	public static final String BTN_OK = "ok";
 	public static final String BTN_CANCEL = "cancel";
-	
+
 	private ExtMessageBoxBehavior handler;
 
 	public ExtMessageDialog(String id) {
 		super(id);
 		init();
 	}
-	
+
 	private void init() {
 		Ext.addContribution(this);
-		
+
 		handler = new ExtMessageBoxBehavior() {
 			@Override
-			public void onClick(AjaxRequestTarget target, String button, String text) { 
-				ExtMessageDialog.this.onClick(button,text);				
+			public void onClick(AjaxRequestTarget target, String button, String text) {
+				ExtMessageDialog.this.onClick(button,text);
 			}
-		} ; 
+		} ;
 		add(handler);
 	}
-	
+
 
 	/**
 	 * Callback handler on message box button click. Ovveride this method to intercept the message button events
-	 * 
+	 *
 	 * @param pressedButton the button pressed on the dialog
 	 * @param text the entered text (only for prompt dialog)
 	 */
 	protected void onClick( final String pressedButton, final String text ) {
-		
+
 	}
 
 	/**
 	 * Displays a <i>Confirm</i> dialog
-	 * 
+	 *
 	 * @param target the current ajax request target
 	 * @param title the dialog string title
 	 * @param message the dialog content message
 	 */
-	public void confim( AjaxRequestTarget target, String title, String message ) { 
+	public void confim( AjaxRequestTarget target, String title, String message ) {
 		Config config = new Config( MessageType.CONFIRM.config() );
 		config.put("title",title);
 		config.put("msg",message);
 		show(target,config);
 	}
-	
+
 	/**
 	 * Display a <i>Prompt</i> Dialog
-	 * 
+	 *
 	 * @param target the current ajax request target
 	 * @param title the prompt dialog title string
 	 * @param message the prompt dialog message string
 	 */
-	public void prompt( AjaxRequestTarget target, String title, String message  ) { 
+	public void prompt( AjaxRequestTarget target, String title, String message  ) {
 		prompt(target,title,message,"", false);
 	}
 
 	/**
 	 * Display a <i>Prompt</i> Dialog
-	 * 
+	 *
 	 * @param target the current ajax request target
 	 * @param title the prompt dialog title string
 	 * @param message the prompt dialog message string
 	 * @param defValue the default value displayed in the input field
 	 * @param isMultiline when <code>true</code> dialog will be display a multiline inout area
 	 */
-	public void prompt( AjaxRequestTarget target, String title, String message, String defValue, boolean isMultiline  ) { 
+	public void prompt( AjaxRequestTarget target, String title, String message, String defValue, boolean isMultiline  ) {
 		Config config = new Config( MessageType.PROMPT.config() );
 		config.put("title",title);
 		config.put("msg",message);
@@ -94,12 +110,12 @@ public class ExtMessageDialog extends WebMarkupContainer {
 
 	/**
 	 * Display an <i>Alert</i> Dialog
-	 * 
+	 *
 	 * @param target the current ajax request target
 	 * @param title the altet dialog title string
 	 * @param message the alert dialog message string
 	 */
-	public void alert( AjaxRequestTarget target, String title, String message  ) { 
+	public void alert( AjaxRequestTarget target, String title, String message  ) {
 		Config config = new Config( MessageType.ALERT.config() );
 		config.put("title",title);
 		config.put("msg",message);
@@ -108,11 +124,11 @@ public class ExtMessageDialog extends WebMarkupContainer {
 
 	/**
 	 * Display a configurable dialog message
-	 * 
+	 *
 	 * @param target the current ajax request target
 	 * @param options configuration object, see <i>Ext</i> documentation for references
 	 */
-	public void show( AjaxRequestTarget target, Config config ) { 
+	public void show( AjaxRequestTarget target, Config config ) {
 		String script = handler.dialogScript(config);
 		target.appendJavascript(script);
 	}
--- wicket-ext/src/org/wicketstuff/extjs/dialog/MessageType.java
+++ wicket-ext/src/org/wicketstuff/extjs/dialog/MessageType.java
@@ -1,16 +1,32 @@
+/*
+ *  Copyright 2008 Wicket-Ext
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.wicketstuff.extjs.dialog;
 
 import org.wicketstuff.extjs.Config;
 import org.wicketstuff.extjs.Ext;
 
 /**
- * Defines configuration for various message box types 
- * 
+ * Defines configuration for various message box types
+ *
  * @author Paolo Di Tommaso
  *
  */
 public enum MessageType {
-	
+
 	INFO() {
 		{
 			config = new Config();
@@ -19,7 +35,7 @@ public enum MessageType {
 			config.set("buttons", Ext.literal("Ext.MessageBox.OK"));
 		}
 	} ,
-	
+
 	ALERT() {
 		{
 			config = new Config();
@@ -28,7 +44,7 @@ public enum MessageType {
 			config.set("buttons", Ext.literal("Ext.MessageBox.OK"));
 		}
 	} ,
-	
+
 	CONFIRM() {
 		{
 			config = new Config();
@@ -37,8 +53,8 @@ public enum MessageType {
 			config.set("buttons", Ext.literal("Ext.MessageBox.YESNO"));
 		}
 	} ,
-	
-	PROMPT() { 
+
+	PROMPT() {
 		{
 			config = new Config();
 			config.set("title", "Prompt");
@@ -46,8 +62,8 @@ public enum MessageType {
 			config.set("buttons", Ext.literal("Ext.MessageBox.OKCANCEL"));
 		}
 	},
-	
-	ERROR() { 
+
+	ERROR() {
 		{
 			config = new Config();
 			config.set("title", "Error");
@@ -59,4 +75,4 @@ public enum MessageType {
 	protected Config config;
 
 	public Config config() { return config; }
-} 
+}
