2b96282f0c0e700a49b88c017ca5b5013f95f22a
paolo.ditommaso <paolo.ditommaso@fab100c0-a45f-11dd-a9c7-49dec45ca499>
2008-12-28 16:02:57 +0000

    improvements & licence headers - ver 0.6

--- trunk/wicket-ext/src/org/wicketstuff/extjs/tabs/ExtTabbedPanel.java
+++ trunk/wicket-ext/src/org/wicketstuff/extjs/tabs/ExtTabbedPanel.java
@@ -1,38 +1,44 @@
+/*
+ *  Copyright 2008 Wicket-Ext
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.wicketstuff.extjs.tabs;
 
-import java.io.Serializable;
-import java.util.Iterator;
-import java.util.List;
-
-import org.apache.wicket.Component;
-import org.apache.wicket.MarkupContainer;
-import org.apache.wicket.markup.html.list.ListView;
-import org.apache.wicket.markup.repeater.AbstractRepeater;
 import org.apache.wicket.model.IModel;
-import org.apache.wicket.model.Model;
+import org.wicketstuff.extjs.ExtContainer;
 import org.wicketstuff.extjs.behavior.ExtTabbedPanelBehavior;
 
-public class ExtTabbedPanel extends MarkupContainer {
+public class ExtTabbedPanel extends ExtContainer {
 
-  /**
-   *
-   */
-  private static final long serialVersionUID = 1L;
+	private boolean autoTagComponents;
+
+	public ExtTabbedPanel(String id, boolean autoTagComponents) {
+		super(id);
+		this.autoTagComponents = autoTagComponents;
+		init();
+	}
 
 
   public ExtTabbedPanel(String id, IModel model, boolean autoTagComponents) {
     super(id, model);
-    init(autoTagComponents);
+	this.autoTagComponents = autoTagComponents;
+	init();
   }
 
 
-  public ExtTabbedPanel(String id, boolean autoTagComponents) {
-    super(id);
-    init(autoTagComponents);
-  }
-
-
-  private void init(boolean autoTagComponents) {
-    add(new ExtTabbedPanelBehavior(autoTagComponents));
+  private void init() {
+    add(new ExtTabbedPanelBehavior(config()).setAutoTagComponents(autoTagComponents)) ;
   }
 }
