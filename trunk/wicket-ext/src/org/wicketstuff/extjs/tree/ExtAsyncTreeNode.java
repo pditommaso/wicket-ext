4cfe32b800a1b01872b74e6dc498f02d28109976
paolo.ditommaso <paolo.ditommaso@fab100c0-a45f-11dd-a9c7-49dec45ca499>
2008-12-28 16:04:00 +0000

    improvements & licence headers - ver 0.6

--- trunk/wicket-ext/src/org/wicketstuff/extjs/tree/ExtAsyncTreeNode.java
+++ trunk/wicket-ext/src/org/wicketstuff/extjs/tree/ExtAsyncTreeNode.java
@@ -1,3 +1,19 @@
+/*
+ *  Copyright 2008 Wicket-Ext
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.wicketstuff.extjs.tree;
 
 import org.wicketstuff.extjs.Config;
@@ -5,17 +21,17 @@ import org.wicketstuff.extjs.ExtClass;
 
 public class ExtAsyncTreeNode extends ExtClass {
 
-	
+
 	private static final long serialVersionUID = 1L;
 	public static final String ROOT_ID = "@root@";
 	private static Config defaultOptions = new Config();
-	
+
 	{
 		defaultOptions.put("draggable", false);
 	}
-	
+
 	private Config config;
-	
+
 	public ExtAsyncTreeNode(String text, String id) {
 		this(text,id,new Config());
 	}
@@ -28,8 +44,8 @@ public class ExtAsyncTreeNode extends ExtClass {
 	}
 
 	@Override
-	public CharSequence newInstance() { 
-		config.putNotExisting(defaultOptions);
+	public CharSequence newInstance() {
+		config.putIfNotExists(defaultOptions);
 		return super.newInstance();
-	}	
+	}
 }
