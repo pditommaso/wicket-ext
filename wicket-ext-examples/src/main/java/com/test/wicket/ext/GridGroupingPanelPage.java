73cc9f5904971e8f7da0b77fcaedae69715675dd
yann.blazart <yann.blazart@fab100c0-a45f-11dd-a9c7-49dec45ca499>
2009-06-02 08:39:42 +0000

    first version of the new maven layout

---
+++ wicket-ext-examples/src/main/java/com/test/wicket/ext/GridGroupingPanelPage.java
@@ -0,0 +1,48 @@
+package com.test.wicket.ext;
+
+import java.util.Iterator;
+
+import org.apache.wicket.markup.html.WebPage;
+import org.wicketstuff.extjs.grid.ColumnMap;
+import org.wicketstuff.extjs.grid.ExtGroupingView;
+
+public class GridGroupingPanelPage extends WebPage {
+
+
+	public GridGroupingPanelPage() {
+
+		add( new ExtGroupingView<DataBean> ("grid") {
+
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			protected ColumnMap[] getColumns() {
+				ColumnMap col1 = new ColumnMap("company", "Company name") .setSortable(true);
+				ColumnMap col2 = new ColumnMap("price", "Price") .setSortable(true);
+				ColumnMap col3 = new ColumnMap("change", "Change");
+				ColumnMap col4 = new ColumnMap("lastUpdate", "Last update");
+				ColumnMap col5 = new ColumnMap("industry", "Industry");
+
+				return new ColumnMap[] { col1, col2, col3, col4, col5 };
+			}
+
+			public Integer totalRecords() { return DataHelper.list().size(); }
+
+
+
+			public Iterator<DataBean> iterator(Query query) {
+				Integer start = query.getStart() != null ? query.getStart() : 0;
+				Integer count = query.getLimit() != null ? query.getLimit() : 100;
+				return DataHelper.list(start, count, query.getSort(), query.getDirection()).iterator();
+
+			}
+		}.setGroupField("industry")
+		 .setTitle("Grouping Panel")
+		 .setFrame(true)
+		 .setAnimCollapse(true)
+		 .setCollapsible(true)
+		 .setWidth(600)
+		 .setHeight(500)
+		);
+	}
+}
---
+++ wicket-ext-examples/src/main/java/com/test/wicket/ext/GridPanelPage.java
@@ -0,0 +1,42 @@
+package com.test.wicket.ext;
+
+import java.util.Iterator;
+
+import org.apache.wicket.markup.html.WebPage;
+import org.wicketstuff.extjs.grid.ColumnMap;
+import org.wicketstuff.extjs.grid.ExtGridView;
+
+public class GridPanelPage extends WebPage {
+
+	public GridPanelPage() {
+
+		add( new ExtGridView<DataBean> ("grid") {
+
+			@Override
+			protected ColumnMap[] getColumns() {
+				ColumnMap col1 = new ColumnMap("company", "Company name") .setSortable(true);
+				ColumnMap col2 = new ColumnMap("price", "Price") .setSortable(true);
+				ColumnMap col3 = new ColumnMap("change", "Change");
+				ColumnMap col4 = new ColumnMap("lastUpdate", "Last update");
+
+				return new ColumnMap[] { col1, col2, col3, col4 };
+			}
+
+			public Integer totalRecords() { return DataHelper.list().size(); }
+
+
+			public Iterator<DataBean> iterator(Query query) {
+				Integer start = query.getStart() != null ? query.getStart() : 0;
+				Integer count = query.getLimit() != null ? query.getLimit() : 100;
+				return DataHelper.list(start, count, query.getSort(), query.getDirection()).iterator();
+
+			}}
+			.setPageSize(10)
+			.setSortField("company")
+		    .setTitle("Sample GridPanel")
+			.setWidth(600)
+			.setHeight(500)
+
+		);
+	}
+}
